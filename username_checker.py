f76a31c8e8f605c90d0379d67ae9fcdd2fbafa95
Milk <105567237+VyIq@users.noreply.github.com>
2023-01-13 02:41:48 -0500

    Update username_checker.py

--- username_checker.py
+++ username_checker.py
@@ -1,9 +1,1 @@
-import pyautogui
-import PIL
 
-with open('usernames.txt', "r") as f:
-username_list = f.read().splitlines()
-
-pyautogui.write(username_list)
-
-image = pyautogui.screenshot(region=0,0, 300, 400))
